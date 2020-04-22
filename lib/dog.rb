ff77980119c240804baab28609861fa4ea5a6a74
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-22 20:22:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,8 @@
 class Dog
 
-  attr_accessor :name, :breed
+  attr_accessor :name, :breed, :id
+  
 
-  def initialize(id = nil, name, breed)
-    @id = id
-    @name = name
-    @breed = breed
-  end
+  
 
 end
